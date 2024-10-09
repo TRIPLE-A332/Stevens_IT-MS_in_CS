036f95c60d25b3a5f9fe16cf1de739013c037f6b
zone1AA <100586875+zone1AA@users.noreply.github.com>
2024-10-09 18:09:14 -0400

    javaSabkaBawa

---
+++ FALL_24/CS501_Java/Practice/Exercise(ungraded)/src/people/Main.java
@@ -0,0 +1,10 @@
+package people;
+
+public class Main {
+    public static void main(String[] args) {
+        Person p1 = new Person("Ali","aaa",2020);
+        System.out.println(p1.canVote());
+        
+    }
+
+}
